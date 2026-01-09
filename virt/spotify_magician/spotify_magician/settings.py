44e14788c023c0b76f085e064a7a4699a3a0db07
Robert <77187829+iRoberTy@users.noreply.github.com>
2026-01-09 18:34:14 +0100

    deploy prob?

--- virt/spotify_magician/spotify_magician/settings.py
+++ virt/spotify_magician/spotify_magician/settings.py
@@ -127,9 +127,9 @@ LOGIN_URL = 'spotify_login'
 SPOTIFY_CLIENT_ID = "b90fcd35292d4b59983b191d99496714"
 SPOTIFY_CLIENT_SECRET = "0110ec5d705f42e396e6f5f91b11ea12"
 SPOTIFY_REDIRECT_URI = "https://127.0.0.1:8000/callback/"
-SESSION_COOKIE_SECURE = True   # Nur über HTTPS senden
-SESSION_COOKIE_HTTPONLY = True # JavaScript kann nicht darauf zugreifen (Schutz vor XSS)
-SESSION_COOKIE_SAMESITE = 'Lax' # Schutz vor CSRF
+#Deploy SESSION_COOKIE_SECURE = True   # Nur über HTTPS senden
+#SESSION_COOKIE_HTTPONLY = True # JavaScript kann nicht darauf zugreifen (Schutz vor XSS)
+#SESSION_COOKIE_SAMESITE = 'Lax' # Schutz vor CSRF
 
 if not DEBUG:
     # Erzwingt HTTPS (leitet http Anfragen auf https um)
