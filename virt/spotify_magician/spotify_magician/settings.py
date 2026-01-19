2d7e2fb120ed2fb247dff5f3bd971249a1a12f76
Robert <77187829+iRoberTy@users.noreply.github.com>
2026-01-19 19:03:59 +0100

    v1.0

--- virt/spotify_magician/spotify_magician/settings.py
+++ virt/spotify_magician/spotify_magician/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'spotify_magician.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
+    'default': dj-database-url.config(
         default='sqlite:///db.sqlite3',
         conn_max_age=600
     )
